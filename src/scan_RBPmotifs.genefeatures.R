1bb49e2e241bd86f76800a748be3b7315b936421
borauyar <bora.uyar@mdc-berlin.de>
2016-05-02 17:53:11 +0200

    finished first draft of known-motif enrichment scripts

--- src/scan_RBPmotifs.genefeatures.R
+++ src/scan_RBPmotifs.genefeatures.R
@@ -15,17 +15,18 @@ RCAS: run RCAS pipeline for a given ENSEMBL annotation file and BED formatted in
 
 Arguments:
 --gtfFile=annotation.gtf            ENSEMBL GTF file
---genome_version=VERSION            genome version; supported values for VERSION are
+--genomeVersion=VERSION            genome version; supported values for VERSION are
 'hg19'
---geneIdsFile='gene_ids.txt'        A txt file containing one ENSEMBL gene id per line
 --knownMotifsDir='<path to RBPmap dir>' Directory containing pssm matrices for known RBP motifs (/home/buyar/datasets/known-motifs/RBP_PSSMs)
+--geneIdsFile='<path to file containing gene ids'> Ensembl format gene ids - one id per line
 --help                              display this help text and exit
 
 Example:
-RCAS --gtfFile=/data/akalin/Base/Annotation/hg19/EnsemblGenes/142812_EnsemblFTP_hg19_EnsemblGenes.gtf \
---genome_version=hg19"
-
-
+Rscript scan_RBPmotifs.genefeatures.R \
+    --gtfFile=~/Desktop/datasets/Ensembl75.hg19.gtf \
+    --genomeVersion='hg19' \
+    --knownMotifsDir=~/Desktop/home/buyar/datasets/known-motifs/RBP_PSSMs \
+    --geneIdsFile=~/Desktop/home/buyar/collaborations/mikuda/motif_discovery/geneSets/Group1_WT_up_EDC4_IKK_up.txt"
 
 ## Help section
 if("--help" %in% args) {
@@ -44,9 +45,14 @@ if(!("gtfFile" %in% argsDF$V1)) {
   stop("provide the path to ENSEMBL gtf file\n")
 }
 
-if(!("genome_version" %in% argsDF$V1)) {
+if(!("genomeVersion" %in% argsDF$V1)) {
   cat(helpCommand, "\n")
   stop("provide the genome version: choose between hg19, mm9, ce6 or dm3\n")
+}
+
+if(!("knownMotifsDir" %in% argsDF$V1)) {
+  cat(helpCommand, "\n")
+  stop("provide a file containing PSSM values for known RBP motifs\n")
 }
 
 if(!("geneIdsFile" %in% argsDF$V1)) {
@@ -56,6 +62,7 @@ if(!("geneIdsFile" %in% argsDF$V1)) {
 
 gtfFile = argsL$gtfFile
 genomeVersion = argsL$genomeVersion
+knownMotifsDir = argsL$knownMotifsDir
 geneIdsFile = argsL$geneIdsFile
 
 if(!genomeVersion %in% c('hg19', 'ce6', 'mm9', 'dm3')){
@@ -83,34 +90,87 @@ parseMotifPWMs <- function (motifsPath) {
   return(pwms)
 }
 
+#features: GRanges object
+#PWMs pwms returned from parseMotifPWMs
+countMotifHits <- function (PWMs, features, genomeVersion) {
 
-geneIds <- scan(file = geneIdsFile, what = character())
+  cat('extracting sequences\n')
+  Seqs <- RCAS::extractSequences(queryRegions = features, genomeVersion = genomeVersion)
 
-#txdbFeatures <- lapply(X = gtfSubsetList, FUN = function (x) { RCAS::getTxdbFeaturesFromGff(gff = x) })
+  pwmHits <- c()
+  for (i in 1:length(PWMs)) {
+    tmm <- PWMs[[i]]
+    motif <- names(PWMs)[i]
+    cat('scanning for motif',motif)
+    nhits <- sum(lengths(lapply(X=Seqs, FUN = function(x) {Biostrings::matchPWM(pwm = tmm, subject = x)})) > 0)
+    cat('=> found ',nhits,'hits\n')
+    pwmHits <- c(pwmHits, nhits)
+  }
+  return (pwmHits)
+}
+
 txdbFeatures <- RCAS::getTxdbFeaturesFromGff(gff = gtf)
 
 #Assign gene ids to the transcripts in the txdbFeatures
 ids <- lapply(X = txdbFeatures, function(x) { m = match(x$tx_name, gtf$transcript_id); gtf[m,]$gene_id})
-for(i in range(length(names(txdbFeatures)))) {
+for(i in 1:length(names(txdbFeatures))) {
   txdbFeatures[[i]]$gene_id <- ids[[i]]
 }
 ##
 
-features <- txdbFeatures$threeUTRs
-reducedFeatures <- GenomicRanges::reduce(GenomicRanges::split(x=features, f = features$gene_id))
+##Get PWMs from files
+PWMs <- parseMotifPWMs(motifsPath = knownMotifsDir)
 
-#features for the input gene list
-foregroundFeatures <- reducedFeatures[names(reducedFeatures) %in% geneIds]
+calculateEnrichedMotifs <- function (features, geneIds, genomeVersion, PWMs) {
+  ##Get foreground and background features
+  reducedFeatures <- GenomicRanges::reduce(GenomicRanges::split(x=features, f = features$gene_id))
+  foregroundFeatures <- unlist(reducedFeatures[names(reducedFeatures) %in% geneIds])
+  allGenes <- names(reducedFeatures)
+  backgroundGenes <- sample(allGenes[!allGenes %in% geneIds], 100)
+  backgroundFeatures <- unlist(reducedFeatures[names(reducedFeatures) %in% backgroundGenes])
 
-#features for the randomly selected list of genes (equal to the number of genes in the foreground list)
-allGenes <- names(reducedFeatures)
-backgroundGenes <- sample(x = allGenes[!allGenes %in% geneIds], size = 1000)
+  fgHits <- countMotifHits(PWMs = PWMs, features = foregroundFeatures, genomeVersion = genomeVersion)
+  bgHits <- countMotifHits(PWMs = PWMs, features = backgroundFeatures, genomeVersion = genomeVersion)
 
-backgroundFeatures <- reducedFeatures[backgroundGenes]
-bgSeqs <- RCAS::extractSequences(queryRegions = unlist(backgroundFeatures), genomeVersion = genomeVersion)
-fgSeqs <- RCAS::extractSequences(queryRegions = unlist(foregroundFeatures), genomeVersion = genomeVersion)
+  fgSize <- length(foregroundFeatures)
+  bgSize <- length(backgroundFeatures)
 
-fgHits <- sum(lengths(lapply(X=fgSeqs, FUN = function(x) {Biostrings::matchPWM(pwm = tmm, subject = x, min.score = '90%')})) > 0)
-bgHits <- sum(lengths(lapply(X=bgSeqs, FUN = function(x) {Biostrings::matchPWM(pwm = tmm, subject = x, min.score = '90%')})) > 0)
+  pvals <- c()
+  for (i in 1:length(PWMs)) {
+    f <- fgHits[i]
+    b <- bgHits[i]
+    contingencyTable <- matrix(c(f, b, fgSize - f, bgSize - b), nrow = 2,
+                               dimnames = list(c("foreground", "background"),
+                                               c("found", "not_found")))
+    p  <- stats::fisher.test(contingencyTable, alternative = "greater")$p.value
+    pvals <- c(pvals, p)
+  }
+
+  results <- data.frame('motifs' = names(PWMs),
+                        'fgHits' = fgHits,
+                        'fgFraction' = round(fgHits/fgSize,2),
+                        'bgHits' = bgHits,
+                        'bgFraction' = round(bgHits/bgSize,2),
+                        'pValue' = pvals)
+  return(results[order(results$pValue),])
+}
+
+results <- calculateEnrichedMotifs(features = txdbFeatures$threeUTRs,
+                                   geneIds = scan(file = geneIdsFile,
+                                                  what = character()),
+                                   genomeVersion = genomeVersion, PWMs = PWMs
+                                   )
+outFile <- paste0(gsub(pattern = '.txt', replacement = '', basename(geneIdsFile)), '.motifscanresults.threeUTRs.tsv')
+
+write.table(x = results, file = outFile, quote = FALSE, row.names = FALSE, sep = '\t')
+
+results <- calculateEnrichedMotifs(features = txdbFeatures$fiveUTRs,
+                                   geneIds = scan(file = geneIdsFile,
+                                                  what = character()),
+                                   genomeVersion = genomeVersion,
+                                   PWMs = PWMs)
+outFile <- paste0(gsub(pattern = '.txt', replacement = '', basename(geneIdsFile)), '.motifscanresults.fiveUTRs.tsv')
+
+write.table(x = results, file = outFile, quote = FALSE, row.names = FALSE, sep = '\t')
 
 
