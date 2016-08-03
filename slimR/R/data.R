7e0063efe09d9ac2e95ce9e824df9a106cd0757f
borauyar <bora.uyar@mdc-berlin.de>
2016-08-03 20:37:30 +0200

    added built in data for amino acids and a sample fasta sequence

---
+++ slimR/R/data.R
@@ -0,0 +1,17 @@
+#' Amino Acid table
+#'
+#' Amino acid table for converting between three letter code and one letter code.
+#'
+#' @return A data.frame object
+#' @format data.frame object with 22 rows and two columns
+"aaTable"
+
+
+#' Sample sequence extracted from a fasta file
+#'
+#' The amino acid sequence for P11166 protein (GLUT1)
+#'
+#' @return A data.frame object
+#' @source Downloaded from uniprot \url{www.uniprot.org/uniprot/P11166.fasta}
+#' @format data.frame object with 22 rows and two columns
+"sampleFasta"
