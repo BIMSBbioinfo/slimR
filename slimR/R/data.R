8738b64a944a1f4d4d4c1651d81302325bb054f9
borauyar <bora.uyar@mdc-berlin.de>
2016-08-05 10:56:02 +0200

    Created built-in dataset motifRegex that contains SLiM regexes from the ELM database

--- slimR/R/data.R
+++ slimR/R/data.R
@@ -23,3 +23,16 @@
 #' @source GLUT1 protein \url{www.uniprot.org/uniprot/P11166}
 #' and humsavar \url{www.uniprot.org/docs/humsavar.txt}
 "sampleVariants"
+
+#' SLiM regular expressions from ELM database
+#'
+#' List of available SLiM classes and their regular expression defined in the
+#' ELM database as of 5th of August, 2016
+#'
+#' This list can be updated using the getElmClasses() function motifRegex <-
+#' as.list(as.vector(elms$RegEx)) names(motifRegex) <-
+#' as.vector(elms$ELM_Identifier)
+#'
+#' @return A list object
+#' @source ELM classes \url{http://elm.eu.org/elms}
+"motifRegex"
