a79ce50e5cee5affd84366af63e8dcde76cb9c82
borauyar <bora.uyar@mdc-berlin.de>
2016-08-12 17:01:53 +0200

    updated mutantSLiMAnalysis - added statistical comparison

--- src/protein_sequence_analysis/mutantSLiMAnalysis.R
+++ src/protein_sequence_analysis/mutantSLiMAnalysis.R
@@ -1,5 +1,6 @@
 #find gain or loss of SLiMs via point mutations in trans-membrane and non-transmembrane proteins
 library('slimR')
+library('data.table')
 #1. Collect arguments
 args <- commandArgs(TRUE)
 
@@ -53,6 +54,49 @@ if(!dir.exists(uniprotDataDir)) {
 }
 
 
+# variants A Granges object of variants parsed from Humsavar using getHumSavar() function or a subset of it with the same structure
+findMotifChangesBulk <- function (variants) {
+
+  ptm <- proc.time()
+
+  motifChanges <- list()
+
+  #load regular expressions for motifs
+  data("motifRegex")
+
+  uniprotAccessions <- unique(as.vector(seqnames(variants)))
+  downloadUniprotFiles(uniprotAccessions = uniprotAccessions, outDir = uniprotDataDir, format = 'fasta')
+
+  pb <- txtProgressBar(min = 0, max = length(uniprotAccessions), style = 3)
+
+  for (i in 1:length(uniprotAccessions)) {
+    setTxtProgressBar(pb, i)
+    uniAcc <- uniprotAccessions[i]
+    fastaFile <- file.path(uniprotDataDir, 'fasta', paste0(uniAcc, '.fasta'))
+
+    if(file.exists(fastaFile)) {
+      sequence <- paste(Biostrings::readAAStringSet(filepath = fastaFile, format = 'fasta'))
+
+      myVariants <- variants[seqnames(variants) == uniAcc]
+
+      df <- data.frame('wtAA' = as.vector(myVariants$wtAA),
+                       'mutAA' = as.vector(myVariants$mutAA),
+                       'pos' = start(myVariants))
+
+      changes <- findMotifChanges(sequence = sequence, variants = df, motifRegex = motifRegex)
+      if (!is.null(changes)) {
+        changes$uniprotAcc <- uniAcc
+        motifChanges[[length(motifChanges)+1]] <- changes
+        names(motifChanges)[length(motifChanges)] <- uniAcc
+      }
+    }
+  }
+
+  close(pb)
+  proc.time() - ptm
+  return(motifChanges)
+}
+
 # read the list of uniprot accessions
 uniList <- scan(file = unilist, what = character())
 
@@ -90,49 +134,50 @@ variants <- getHumSavar()
 overlaps <- GenomicRanges::findOverlaps(query = variants, subject = ctmRegions)
 variantsFiltered <- variants[queryHits(overlaps),]
 ################# findMotifChanges due to mutations for a given list of uniprot accessions
+#motif changes due to disease mutations (that overlap cytosolic regions of trans-membrane proteins)
+diseaseVariants <- variantsFiltered[variantsFiltered$variant == 'Disease',]
+mcD <- data.table(do.call(rbind, findMotifChangesBulk(variants = diseaseVariants)))
+#motif changes due to polymorphisms  (that overlap cytosolic regions of trans-membrane proteins)
+polymorphismVariants <- variantsFiltered[variantsFiltered$variant == 'Polymorphism',]
+mcP <- data.table(do.call(rbind, findMotifChangesBulk(variants = polymorphismVariants)))
 
-
-# variants A Granges object of variants parsed from Humsavar using getHumSavar() function or a subset of it with the same structure
-findMotifChangesBulk <- function (variants) {
-
-  ptm <- proc.time()
-
-  motifChanges <- list()
-
-  #load regular expressions for motifs
-  data("motifRegex")
-
-  uniprotAccessions <- unique(as.vector(seqnames(variants)))
-  downloadUniprotFiles(uniprotAccessions = uniprotAccessions, outDir = uniprotDataDir, format = 'fasta')
-
-  pb <- txtProgressBar(min = 0, max = length(uniprotAccessions), style = 3)
-
-  for (i in 1:length(uniprotAccessions)) {
-    setTxtProgressBar(pb, i)
-    uniAcc <- uniprotAccessions[i]
-    fastaFile <- file.path(uniprotDataDir, 'fasta', paste0(uniAcc, '.fasta'))
-
-    if(file.exists(fastaFile)) {
-      sequence <- paste(Biostrings::readAAStringSet(filepath = fastaFile, format = 'fasta'))
-
-      myVariants <- variants[seqnames(variants) == uniAcc]
-
-      df <- data.frame('wtAA' = as.vector(myVariants$wtAA),
-                       'mutAA' = as.vector(myVariants$mutAA),
-                       'pos' = start(myVariants))
-
-      changes <- findMotifChanges(sequence = sequence, variants = df, motifRegex = motifRegex)
-      if (!is.null(changes)) {
-        motifChanges[[length(motifChanges)+1]] <- changes
-        names(motifChanges)[length(motifChanges)] <- uniAcc
-      }
-    }
-  }
-
-  close(pb)
-  proc.time() - ptm
-  return(motifChanges)
-}
 #################TODO: compare frequency of loss/gain of motifs in disease vs polymorphism datasets
+lostD <- mcD[mcD$change == 'lost',]
+gainedD <- mcD[mcD$change == 'gained',]
+
+lostP <- mcP[mcP$change == 'lost',]
+gainedP <- mcP[mcP$change == 'gained',]
+
+lost <- merge(lostD[,length(change), by=SLiM], lostP[,length(change), by=SLiM], by = 'SLiM', all = TRUE)
+colnames(lost) <- c('SLiM', 'diseaseSuccess', 'polymorphismSuccess')
+lost[is.na(lost)] <- 0
+lost$diseaseFail <- length(diseaseVariants) - lost$diseaseSuccess
+lost$polymorphismFail <- length(polymorphismVariants) - lost$polymorphismSuccess
+
+gained <- merge(gainedD[,length(change), by=SLiM], gainedP[,length(change), by=SLiM], by = 'SLiM', all = TRUE)
+colnames(gained) <- c('SLiM', 'diseaseSuccess', 'polymorphismSuccess')
+gained[is.na(gained)] <- 0
+gained$diseaseFail <- length(diseaseVariants) - gained$diseaseSuccess
+gained$polymorphismFail <- length(polymorphismVariants) - gained$polymorphismSuccess
+
+lost$pVal <-  apply(lost, 1, FUN = function(x) {
+  contingencyTable <- matrix(as.integer(x[2:5]), nrow = 2,
+                             dimnames = list(c("disease", "polymorphism"),
+                                             c("success", "fail")))
+  pVal <- fisher.test(x = contingencyTable, alternative = 'greater')$p.value
+})
+
+gained$pVal <- apply(gained, 1, FUN = function(x) {
+  contingencyTable <- matrix(as.integer(x[2:5]), nrow = 2,
+                             dimnames = list(c("disease", "polymorphism"),
+                                             c("success", "fail")))
+  pVal <- fisher.test(x = contingencyTable, alternative = 'greater')$p.value
+})
+
+lost <- lost[order(pVal)]
+gained <- gained[order(pVal)]
+
+write.table(x = lost, file = 'lost_motifs.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
+write.table(x = gained, file = 'gained_motifs.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
 
 
