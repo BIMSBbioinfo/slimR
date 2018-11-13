33ce94fcb57867d4488b6ff781ae966dd0f0f21b
borauyar <bora.uyar@mdc-berlin.de>
2018-11-13 10:07:29 +0100

    add test for clinvar variants

--- tests/testthat/test_getData.R
+++ tests/testthat/test_getData.R
@@ -1,11 +1,25 @@
 context("Testing data retrieval functions")
 library(slimR)
-hs <- getHumSavar(outdir = getwd())
 
+
+hs <- getHumSavar(outdir = getwd())
 test_that("humsavar variants",
           expect_equal(length(hs) > 78000, TRUE),
           expect_equal(colnames(mcols(hs)), c('geneName', 'FTId', 'change', 'variant',
                                               'dbSNP', 'diseaseName', 'wtAA', 'mutAA')),
           expect_is(hs, 'GRanges'))
+
+
+cv <- getClinVarData()
+#read first 100 lines
+cv_head <- data.table::fread(text = readLines(cv, n = 100))
+test_that("clinvar_variants",
+          expect_equal(cv, "variant_summary.txt.gz"),
+          expect_equal(file.exists(cv), TRUE),
+          expect_equal(ncol(cv_head), 31),
+          expect_equal(colnames(cv_head)[1], "#AlleleID"))
+
+
+
 
 
